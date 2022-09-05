aca2e08363ae1bd8668c2b0601c69b0fd1bff736
wang_buhui <361385654@qq.com>
2022-09-05 17:55:04 +0800

    修改存储数据逻辑，修改预览界面数据处理

--- checkSpine/previewCurve.py
+++ checkSpine/previewCurve.py
@@ -37,15 +37,11 @@ def checkTexure(root,file):
                     continue
             except:
                 pass
-            try:
-                index = line.index('size: ')
-                if pngObj[nowPng] and index > -1 :
-                    pngObj[nowPng]['size'] = replaceStr(line)[index + 6:]
-            except:
-                nowPng = ''
         return pngObj
     except:
         return False
+def replaceStr(str):
+    return str.replace("\n","").replace("\r","")
 
 # 检测json文件
 def checkJson(cfg,root,file):
@@ -58,12 +54,13 @@ def checkJson(cfg,root,file):
         obj = json.loads(fs.read())
         sk = obj["skeleton"]
         cfg[pname] = {}
+        cfg[pname]['name'] = pname
         cfg[pname]['atlas'] = os.path.join(root,pname+'.atlas').decode('gbk')
         cfg[pname]['json'] = os.path.join(root,pname+'.json').decode('gbk')
-        cfg[pname]['png'] = []
+        cfg[pname]['png'] = {}
         pngStr = checkTexure(root,file)
         for key in pngStr:
-            cfg[pname]['png'].append(os.path.join(root,key).decode('gbk'))
+            cfg[pname]['png'][replaceStr(key)] = os.path.join(root, pngStr[key]['png']).decode('gbk')
         if not pngStr:
             return False
         succ = True
@@ -76,17 +73,24 @@ checkJson(cfg,os.path.dirname(sFilePath),os.path.basename(sFilePath))
 
 import base64
 for spine in cfg:
-    print(cfg[spine]['atlas'])
-    print(cfg[spine]['json'])
-    print(cfg[spine]['png'])
     f = open(cfg[spine]['atlas'],'rb')
-    print(base64.b64encode(f.read()))
+    cfg[spine]['atlas']=base64.b64encode(f.read())
     f.close()
-# batPath = os.path.join(os.path.dirname(sys.argv[0]),'index.html')
-# f = open(batPath,'r+')
-# flist=f.readlines()
-# f=open(batPath,'w+')
-# flist[174]=u'<script>window.previewSpinePath='+json.dumps(cfg,ensure_ascii=False)+'</script>\n'
-# f.writelines(flist)
-# f.close()
-# webbrowser.open(batPath)
+    f = open(cfg[spine]['json'],'rb')
+    cfg[spine]['json']=f.read()
+    f.close()
+    s = {}
+    for key in cfg[spine]['png']:
+        f = open(cfg[spine]['png'][key],'rb')
+        s[key] = base64.b64encode(f.read()) 
+    cfg[spine]['png'] = str(s)
+    f.close()
+batPath = os.path.join(os.path.dirname(sys.argv[0]),'index.html')
+f = open(batPath,'r+')
+flist=f.readlines()
+f.close()
+f = open(batPath,'w+')
+flist[178]='window.previewSpinePath='+json.dumps(cfg)+'\n'
+f.writelines(flist)
+f.close()
+webbrowser.open(batPath)
