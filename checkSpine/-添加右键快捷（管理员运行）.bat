5934dae45abf20436318a880a2eed395c0008a26
buhui_wang <32084033+a6166231@users.noreply.github.com>
2023-02-21 18:39:03 +0800

    修改插入右键命令
    文件路径有空格的时候会导致bat传递的参数被空格打断，误认为多个参数

--- checkSpine/-添加右键快捷（管理员运行）.bat
+++ checkSpine/-添加右键快捷（管理员运行）.bat
@@ -3,5 +3,5 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Directory\background\shell\www" /v 
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Directory\background\shell\www\shell\checkSpine\command" /d %~dp0\checkSp.exe /f
 
 reg add "HKEY_CLASSES_ROOT\SystemFileAssociations\.json\shell\www" /v SubCommands /d "" /f
-reg add "HKEY_CLASSES_ROOT\SystemFileAssociations\.json\shell\www\shell\previewCurve\command" /d "%~dp0\previewCurve.exe %%1" /f
-reg add "HKEY_CLASSES_ROOT\SystemFileAssociations\.json\shell\www\shell\checkSpine\command" /d "%~dp0\checkSp.exe %%1" /f
+reg add "HKEY_CLASSES_ROOT\SystemFileAssociations\.json\shell\www\shell\previewCurve\command" /d "%~dp0\previewCurve.exe "^"%%1""^" /f
+reg add "HKEY_CLASSES_ROOT\SystemFileAssociations\.json\shell\www\shell\checkSpine\command" /d "%~dp0\checkSp.exe "^"%%1""^" /f
